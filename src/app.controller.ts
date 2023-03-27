e93467a05699b5c94203bdb752ff18e1ef80bb71
Denovo <vo0v0000@naver.com>
2023-03-27 20:40:59 +0900

    [DOC] 예시 app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,4 +13,55 @@ export class AppController {
 
     return new ApiResponse(apiResponeStatus.SUCCESS, result);
   }
+
+  /** 독 예시 */
+  // @ApiOperation({
+  //   summary: '상품 생성 API',
+  //   description: `
+  //     상품 생성 API입니다
+      
+  //     필요한정보.
+  //     - name: 상품 이름 - String
+  //     - price: 상품 가격 - BigInt
+  //     - introduce: 상품 소개 - String
+  //     - quantity: 상품 개수 - Number
+  //     - product_status: Enum (E_ProductStatus)
+  //     - archive_id: 아카이브 id - String ULID
+  //     - category_id: 카테고리 id - String ULID
+  //   `,
+  // })
+  // @ApiBody({
+  //   schema: { $ref: getSchemaPath(ProductCreateInputDTO) },
+  //   examples: {
+  //     PRODUCT: {
+  //       description: `상품생성 예시`,
+  //       value: productCreateInputEX,
+  //     },
+  //   },
+  // })
+  // @ApiResponseDTO(
+  //   201,
+  //   new BaseResponse(baseResponeStatus.SUCCESS, productCreateResponseEX),
+  //   '성공',
+  // )
+  // @ApiResponseDTO(
+  //   400.2,
+  //   baseResponeStatus.ARCHIVE_NOT_EXIST,
+  //   '아카이브가 존재하지 않을때',
+  // )
+  // @ApiResponseDTO(
+  //   400.1,
+  //   baseResponeStatus.CATEGORY_NOT_EXIST,
+  //   '카테고리가 존재하지 않을때',
+  // )
+  // @UseGuards(JwtAccessAuthGuard, AdminAuthGuard)
+  // @Post('/')
+  // async createProduct(@Body() productCreateInputDTO: ProductCreateInputDTO) {
+  //   const result = await this.productService.createProduct(
+  //     productCreateInputDTO,
+  //   );
+
+  //   return new BaseResponse(baseResponeStatus.SUCCESS, result);
+  // }
+
 }
