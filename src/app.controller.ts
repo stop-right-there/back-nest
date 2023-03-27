7997bff8e95d60c18aa641fef11e54658f5056ae
Denovo <vo0v0000@naver.com>
2023-03-27 23:27:06 +0900

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  //test
   @Get()
   getHello(): ApiResponse<string> {
     const result = this.appService.getHello();
