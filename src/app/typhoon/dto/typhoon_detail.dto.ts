b331663152650d70eb6b7f481773c316299bbb13
yunseo323 <jokani3244833@gmail.com>
2023-03-29 18:49:11 +0900

    [FEAT]#7 typhoon dto 작성

---
+++ src/app/typhoon/dto/typhoon_detail.dto.ts
@@ -0,0 +1,68 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsNumber } from 'class-validator';
+
+export class TyphoonDetailDTO {
+  @ApiProperty({
+    name: 'typhoon_id',
+    description: '태풍 id',
+    type: 'number',
+  })
+  @IsNumber()
+  typhoon_id: number;
+
+  @ApiProperty({
+    name: 'datetime',
+    description: '현재(최근) 태풍 날짜시간 정보',
+    type: 'Date',
+  })
+  @IsDateString()
+  datetime: number;
+
+  @ApiProperty({
+    name: 'central_latitude',
+    description: '태풍 중심 위도',
+    type: 'number',
+  })
+  @IsNumber()
+  central_latitude: number;
+
+  @ApiProperty({
+    name: 'central_longitude',
+    description: '태풍 중심 경도',
+    type: 'number',
+  })
+  @IsNumber()
+  central_longitude: number;
+
+  @ApiProperty({
+    name: 'central_pressure',
+    description: '태풍 중심 기압',
+    type: 'number',
+  })
+  @IsNumber()
+  central_pressure: number;
+
+  @ApiProperty({
+    name: 'maximum_wind_speed',
+    description: '태풍 최대 풍속',
+    type: 'number',
+  })
+  @IsNumber()
+  maximum_wind_speed: number;
+
+  @ApiProperty({
+    name: 'wind_radius',
+    description: '태풍 풍속 반경',
+    type: 'number',
+  })
+  @IsNumber()
+  wind_radius?: number;
+
+  @ApiProperty({
+    name: 'grade',
+    description: '태풍 등급',
+    type: 'number',
+  })
+  @IsNumber()
+  grade: number;
+}
