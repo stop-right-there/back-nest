b425b2c1bfbda7063f18600da2c217e0e2bac411
Denovo <vo0v0000@naver.com>
2023-04-04 23:25:10 +0900

    [DOC]#9 태풍 업데이트 이벤트 설명(흐름도) 작성

--- src/app/typhoon/listener/typhoon-updated.listener.ts
+++ src/app/typhoon/listener/typhoon-updated.listener.ts
@@ -13,6 +13,37 @@ export class TyphoonUpdatedListener {
   private readonly logger = new Logger(TyphoonUpdatedListener.name);
   constructor(private readonly httpService: HttpService) {}
 
+  /**
+   * GDACS에서 태풍정보가 업데이트될시 실행되는 이벤트입니다.
+   *
+   * 1. 태풍이 DB에 존재하는지 확인합니다.
+   *
+   * -- 태풍이 존재하지않을경우
+   * 2. 태풍을 DB에 저장합니다.
+   * 3, 태풍 중심부 날씨 정보를 가져옵니다.
+   * 4. 중심부 날씨를 기반으로 태풍 Detail 정보를 DB에 저장합니다.
+   * 5  태풍 예측을 위한 주변부 날씨를 가져옵니다.
+   * 6. 가져온 주변 날씨 정보를 DB에 저장합니다.
+   * 7. 주변날씨를 예측하기위해 AI 서버로 요청을 보냅니다.
+   * 8. AI 서버로부터 응답받은 정보를 DB에 저장합니다.
+   *
+   *
+   * -- 이미 DB에 태풍이 존재 할 경우
+   * --- 태풍이 존재는 하나, 가장 최근 태풍 Detail의 관찰 날짜와 event로 넘어온 태풍의 관찰날짜가 다른경우
+   * 2. 태풍 중심부 날씨 정보를 가져옵니다.
+   * 3. 중심부 날씨를 기반으로 태풍 Detail 정보를 DB에 저장합니다. (관찰날짜가 다르기때문에 아직 태풍이 활동함을 의미)
+   * 4. 태풍 예측을 위한 주변부 날씨를 가져옵니다.
+   * 5. 가져온 주변 날씨 정보를 DB에 저장합니다.
+   * 6. 주변날씨를 예측하기위해 AI 서버로 요청을 보냅니다.
+   * 7. AI 서버로부터 응답받은 정보를 DB에 저장합니다.
+   *
+   *
+   * --- 태풍이 존재는 하나, 가장 최근 태풍 Detail의 관찰 날짜와 event로 넘어온 태풍의 관찰날짜가 같은경우.
+   * 2. 관찰 날짜가 같기때문에 태풍이 활동을 멈춘것을 의미합니다.
+   * 3. 해당 태풍의 종료날짜를 업데이트합니다.
+   * 이때 태풍종료가 되었기때문에 날씨 정보 및 예측을 받을 필요가 없습니다.
+   *
+   */
   @OnEvent('typhoon.updated')
   async handleTyphoonUpdatedEvent(event: GDASCTyphoonUpdatedEvent) {
     this.logger.log(
@@ -43,12 +74,16 @@ export class TyphoonUpdatedListener {
       maximum_wind_speed,
       grade,
     );
+
+    // ==============================================================
     // 태풍이 DB에 존재하지 않을경우 추가하기위해 태풍 DTO를 생성합니다.
     const typhoonDTO: TyphoonDTO = {
       name,
       typhoon_id: id,
       start_date: start_date.toUTCString(),
     };
+
+    // ==============================================================
 
     //태풍 중심부 기압을 받아옵니다.
     //관측은 과거에 이루어지기에 timemachine을 사용합니다.
@@ -154,8 +189,5 @@ export class TyphoonUpdatedListener {
       //   console.log(arroundWeaher);
     });
     //=================================================================================================
-
-    //   await this.httpService.axiosRef.get();
-    // const arroundWeaher = await this.httpService.axiosRef.get();
   }
 }
