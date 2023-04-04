f99ddf53d68f1b4a9d8f1b80d67cc3b5509662df
Denovo <vo0v0000@naver.com>
2023-04-04 23:58:30 +0900

    [DOC]#9 태풍 발생 이벤트 코드 설명 추가

--- src/app/typhoon/listener/typhoon-updated.listener.ts
+++ src/app/typhoon/listener/typhoon-updated.listener.ts
@@ -7,6 +7,7 @@ import { TyphoonDTO } from '../dto/typhoon.dto';
 import { TyphoonAroundWeatherDTO } from '../dto/typhoon_arount_weather.dto';
 import { TyphoonDetailDTO } from '../dto/typhoon_detail.dto';
 import { GDASCTyphoonUpdatedEvent } from '../event/typhoon-updated.envent';
+import { typhoon_HINAMNOR } from '../mock/typhoon.mock';
 
 @Injectable()
 export class TyphoonUpdatedListener {
@@ -51,11 +52,12 @@ export class TyphoonUpdatedListener {
     );
     const { id, date } = event;
 
+    // =========================GDACS 태풍 정보 가져오기=========================
+    // GDACS 사이트에서 XML을 가져온 후 태풍 detail 정보를 파싱합니다.
     const { data: xml } = await this.httpService.axiosRef.get(
       `https://www.gdacs.org/datareport/resources/TC/${id}/rss_${id}.xml`,
     );
 
-    // GDACS 사이트에서 제공하는 XML에서  태풍 detail 정보를 파싱합니다.
     const {
       name,
       central_latitude,
@@ -65,41 +67,58 @@ export class TyphoonUpdatedListener {
       maximum_wind_speed,
       grade,
     } = parseGDACS_XML(xml);
-    console.log(
-      name,
-      central_latitude,
-      central_longitude,
-      observation_date,
-      start_date,
-      maximum_wind_speed,
-      grade,
-    );
+    // ======================================================================
 
-    // ==============================================================
-    // 태풍이 DB에 존재하지 않을경우 추가하기위해 태풍 DTO를 생성합니다.
-    const typhoonDTO: TyphoonDTO = {
-      name,
-      typhoon_id: id,
-      start_date: start_date.toUTCString(),
-    };
+    // DB에 태풍이 존재하는지 확인합니다.
+    // 현재는 DB를 구축하지 않았기에 임시로 값을 설정합니다.
+    // const exist = false;          //DB조회 코드 DB에 존재하지않는다고 가정.
+    const exist = typhoon_HINAMNOR; //DB조회 코드 DB에 존재한다고가정.
 
-    // ==============================================================
+    // ======================DB에 태풍이 존재하지 않음=======================
+    if (!exist) {
+      // 태풍이 DB에 존재하지 않을경우 추가하기위해 태풍 DTO를 생성합니다.
+      const typhoonDTO: TyphoonDTO = {
+        name,
+        typhoon_id: id,
+        start_date: start_date.toUTCString(),
+      };
+
+      // DB에 태풍을 추가합니다.
+      /**
+       * TODO. 태풍을 추가하는 코드 작성
+       */
+    }
+    // =================================================================
+
+    // ======================DB에 태풍이 존재하고, 관찰 날짜가 같음=======================
+    // TODO 최근날 짜와 비교하는 조건 추가
+    if (exist.historical_details.length > 0) {
+      this.logger.log(
+        `종료된 태풍 :: typhoon_id: ${event.id}, envent_date: ${event.date}`,
+      );
+      // 태풍이 DB에 존재하고, 관찰 날짜가 같을경우 태풍이 종료된것을 의미합니다.
+      // 따라서 태풍의 종료날짜를 업데이트합니다.
+      // TODO. 태풍 종료날짜 업데이트 코드 작성
+      /**
+       * 태풍 종료날짜 업데이트 코드 작성
+       */
+
+      return; // 이후 예측 및 날씨정보 업데이트를 하지 않습니다.
+    }
+    // ===========================================================================
+
+    // ======================DB에 태풍이 존재하고, 관찰 날짜가 다름=======================
+    // 아래 코드중 날씨와 관련된 코드는 모두 weather service로 변경해야합니다..
 
     //태풍 중심부 기압을 받아옵니다.
     //관측은 과거에 이루어지기에 timemachine을 사용합니다.
-    const { data: openWeatherMapRes } = await this.httpService.axiosRef
-      .get(
-        `https://api.openweathermap.org/data/3.0/onecall/timemachine?dt=${
-          observation_date.getTime() / 1000
-        }&lat=${central_latitude}&lon=${central_longitude}&appid=${
-          process.env.OPEN_WEATHER_API_KEY
-        }&type=hour`,
-      )
-      .catch((e) => {
-        console.log(e.message);
-        `1`;
-        return { data: {} };
-      });
+    const { data: openWeatherMapRes } = await this.httpService.axiosRef.get(
+      `https://api.openweathermap.org/data/3.0/onecall/timemachine?dt=${
+        observation_date.getTime() / 1000
+      }&lat=${central_latitude}&lon=${central_longitude}&appid=${
+        process.env.OPEN_WEATHER_API_KEY
+      }&type=hour`,
+    );
 
     const { data: weatherData } = openWeatherMapRes;
     const { pressure } = weatherData[0];
@@ -123,10 +142,10 @@ export class TyphoonUpdatedListener {
     const day = date.getUTCDate().toString();
     const hour = date.getUTCHours().toString();
 
+    // ******************************날씨 서비스로 교체해야할부분********************************
     // 이후 날씨 서비스에 있는 조회 메소드를 이용하여  ninePoint에 대한 날씨 정보를 불러옵니다.
     // 날씨 서비스가 완성되면 수정을 해야합니다.
     // 날씨 서비스를 연동하기전까지는 axioService를 이용하여 날씨 정보를 불러옵니다.
-    // ================================날씨 서비스로 교체해야할부분============================================
     const arroundWeaherList: TyphoonAroundWeatherDTO[] = await Promise.all(
       ninePoint.map(async ({ latitude, longitude }, index) => {
         const { data } = await this.httpService.axiosRef.get(
@@ -180,14 +199,14 @@ export class TyphoonUpdatedListener {
         };
       }),
     );
-    // =================================================================================================
+    // ***********************************************************************************
 
+    // ********************************주변 날씨를 저장하는 코드 작성해야할부분********************************
     // 태풍 주변 날씨 정보를 저장하는 코드를 작성해야합니다. 아직 DB를 구축하지 않았기에 생략합니다.
-    //=================================주변 날씨를 저장하는 코드 작성해야할부분==================================
     arroundWeaherList.forEach((arroundWeaher) => {
       //arroundWeaher
       //   console.log(arroundWeaher);
     });
-    //=================================================================================================
+    // **********************************************************************************************
   }
 }
