50428aa21324bb44f8d1ae852d391c818902fcb9
Denovo <vo0v0000@naver.com>
2023-04-05 01:47:35 +0900

    [FEAT]#36 프리즈마 연결, 프리즈마 모델 구축, --목업데이터 디비 insert완료

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({ log: [{ emit: 'stdout', level: 'query' }], errorFormat: 'pretty' });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+
+    this.$use(async (params, next) => {
+      console.log(params);
+
+      const result = await next(params);
+      return result;
+    });
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
