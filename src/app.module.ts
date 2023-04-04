170e1f212b006bdadfa28b54666899d4d0dac6ac
Denovo <vo0v0000@naver.com>
2023-04-05 01:47:35 +0900

    [FEAT]#36 프리즈마 연결, 프리즈마 모델 구축, --목업데이터 디비 insert완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,15 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configOption } from './common/option/config.option';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [
     // config module
     ConfigModule.forRoot(configOption),
-
     //이벤트 모듈
     EventEmitterModule.forRoot(),
+    // Prisam module
+    PrismaModule,
     TyphoonModule,
     WeatherModule,
   ],
