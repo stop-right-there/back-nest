b1bdac310fb2e53d0f62aa01b33851a05cfe2aa3
Denovo <vo0v0000@naver.com>
2023-04-11 18:49:44 +0900

    [FEAT]#9 aerisweather api res type 작성

---
+++ src/app/typhoon/type/aerisweather.type.ts
@@ -0,0 +1,188 @@
+import { IPosition } from './aerisweather.type';
+export const aresWeatherURL = ({
+  client_id,
+  client_secret,
+}: {
+  client_id: string;
+  client_secret: string;
+}) =>
+  `https://api.aerisapi.com/tropicalcyclones?p=&filter=all&limit=10&client_id=${client_id}&client_secret=${client_secret}`;
+
+export interface IAresWeatherResponse {
+  success: boolean;
+  error: any;
+  response: IAresWeatherData[];
+}
+
+export interface IAresWeatherData {
+  id: string;
+  profile: IAresWeatherTCProfile;
+  position: IPosition;
+  track: ITrack[];
+  forcast: IForecast[];
+  breackPointAlerts: IBreakPointAlerts[];
+  errorCone: IErrorCone;
+  relativeTo: IRelativeTo;
+}
+
+export interface IAresWeatherTCProfile {
+  name: string;
+  year: number;
+  basinOrigin: string;
+  basinCurrent: string;
+  basins: [];
+  event: number;
+  isActive: boolean;
+  lifespan: ILifespan;
+  maxStormType: MaxStormType;
+  maxStormCat: MaxStormCat;
+  windSpeed: IWindSpeed;
+  pressure: IPressure;
+
+  boundingBox: number[];
+  tz: string;
+}
+
+export interface ILifespan {
+  startTimestamp: number;
+  startDateTimeISO: string;
+  endTimestamp: number;
+  endDateTimeISO: string;
+}
+export type MaxStormType = 'TD' | 'TS' | 'H' | 'TY';
+export type MaxStormCat =
+  | 'TD'
+  | 'TS'
+  | 'H1'
+  | 'H2'
+  | 'H3'
+  | 'H4'
+  | 'H5'
+  | 'TY'
+  | 'STY';
+export interface IWindSpeed {
+  maxKTS: number;
+  maxKPH: number;
+  maxMPH: number;
+  maxTimestamp: number;
+  maxDateTimeISO: string;
+}
+
+export interface IPressure {
+  minMB: number;
+  minIn: number;
+  minTimestamp: number;
+  minDateTimeISO: string;
+}
+
+export interface IPosition {
+  timestamp: number;
+  dateTimeISO: string;
+  location: ILocation;
+  loc: ILoc;
+  details: IDetails;
+}
+
+export interface ILocation {
+  type: string;
+  coordinates: number[];
+}
+export interface ILoc {
+  long: number;
+  lat: number;
+}
+export interface IDetails {
+  stormType: MaxStormType;
+  stormCat: MaxStormCat;
+  stormName: string;
+  stormShortName: string;
+  advisoryNumber: string;
+  basin: string;
+  movement: IMovement;
+  windSpeedKTS: number;
+  windSpeedKPH: number;
+  windSpeedMPH: number;
+  gustSpeedKTS: number;
+  gustSpeedKPH: number;
+  gustSpeedMPH: number;
+  pressureMB: number;
+  pressureIN: number;
+  windRadii: IPositionDetailsWindRadii[];
+}
+
+export interface IMovement {
+  direction: string;
+  directionDEG: number;
+  speedKTS: number;
+  speedKPH: number;
+  speedMPH: number;
+}
+
+export interface IPositionDetailsWindRadii {
+  windSpeedKTS: number;
+  windSpeedKPH: number;
+  windSpeedMPH: number;
+  quadrants: IWindRadii;
+  windField?: any;
+}
+
+interface IQuadrantPosition {
+  long: number;
+  lat: number;
+}
+
+interface IQuadrantDistance {
+  distanceKM: number;
+  distanceMI: number;
+  distanceNM: number;
+}
+
+interface Quadrant {
+  distance: IQuadrantDistance;
+  loc: IQuadrantPosition;
+}
+
+interface IWindRadii {
+  ne: Quadrant;
+  se: Quadrant;
+  sw: Quadrant;
+  nw: Quadrant;
+}
+
+interface ITrack {
+  timestamp: number;
+  dateTimeISO: string;
+  location: ILocation;
+  loc: ILoc;
+  details: IDetails;
+}
+interface IForecast {
+  timestamp: number;
+  dateTimeISO: string;
+  location: ILocation;
+  loc: ILoc;
+  details: IDetails;
+}
+
+interface IErrorCone {
+  Type: string;
+  coordinates: number[];
+}
+interface IBreakPointAlerts {
+  /**
+TR.A = Tropical Storm Watch
+TR.W = Tropical Storm Warning
+HU.A = Hurricane Watch
+HU.W = Hurricane Warning
+*/
+  alertType: string;
+  coords: IErrorCone;
+}
+interface IRelativeTo {
+  lat: number;
+  long: number;
+  bearing: number;
+  bearingEng: string;
+  distanceKM: number;
+  distanceMI: number;
+}
